437f5ef7a3a6bac71154e722732769ce0b575c25
marin72-com <sifon1994@gmail.com>
2016-04-01 18:03:43 +0900

    adopt class

--- word2vec.py
+++ word2vec.py
@@ -6,30 +6,34 @@ import sys
 # output log
 logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
 
-def load_wakati(filename):
-    sentences = word2vec.Text8Corpus(filename)
-    return word2vec.Word2Vec(sentences, size=200, min_count=20, window=10)
-
-def save_model(model, filename='sample.model'):
-    model.save(filename)
-
-def get_word2vec_model():
-    filename = 'tweets-wakati.txt'
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-    model = load_wakati(filename)
-    save_model(model, filename + '.model')
-    return model
+class Word2VecModel:
+    def __init__(self, filename='tweets-wakati.txt'):
+        self.model = self.load_wakati(filename)
     
-def get_similar_words(posi, nega=[], n=10):
-    model = get_word2vec_model()
-    cnt = 1
-    result = model.most_similar(positive = posi, negative = nega, topn = n)
-    for r in result:
-        print cnt, ' ', r[0], ' ', r[1]
-        cnt += 1
+    def load_wakati(self, filename):
+        sentences = word2vec.Text8Corpus(filename)
+        return word2vec.Word2Vec(sentences, size=200, min_count=20, window=10)
+    
+    def save_model(self, filename='sample.model'):
+        self.model.save(filename)
+    
+    def get_word2vec_model(self):
+        return self.model
+        
+    def get_similar_words(self, posi, nega=[], n=10):
+        cnt = 1
+        result = self.model.most_similar(positive = posi, negative = nega, topn = n)
+        print posi
+        for r in result:
+            print cnt, ' ', r[0], ' ', r[1]
+            cnt += 1
 
 if __name__ == '__main__':
     print "Load finish"
-    get_similar_words(['twitter'])
+
+    filename = 'tweets-wakati.txt'
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    wvm = Word2VecModel(filename)
+    wvm.get_similar_words('twitter')
 
